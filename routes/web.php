df103ee572038f1d8691c6d46b054d6848052867
hoangtris <hoangtri.ngo.117@gmail.com>
2020-10-01 10:32:09 +0700

    dang nhap dang ki

--- routes/web.php
+++ routes/web.php
@@ -50,16 +50,8 @@ Route::delete('articles/{id}', ['as' => 'articles.destroy', 'uses' => 'ArticlesC
 Route::resource('articles', 'ArticlesController');
 
 
-// Authentication routes...
-Route::get('auth/login', 'Auth\AuthController@getLogin');
-Route::post('auth/login', 'Auth\AuthController@postLogin');
-Route::get('auth/logout', 'Auth\AuthController@getLogout');
- 
-// Registration routes...
-Route::get('auth/register', 'Auth\AuthController@getRegister');
-Route::post('auth/register', 'Auth\AuthController@postRegister');
-
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
